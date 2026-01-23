0ee6f1a542e668b1281fa8d10925dea72268a027
Suraj Saini <suraj88658917@gmail.com>
2026-01-23 17:12:27 +0530

    Initial commit

---
+++ src/screens/authScreens/OnbordingScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const OnbordingScreen = () => {
+  return (
+    <View>
+      <Text>OnbordingScreen</Text>
+    </View>
+  )
+}
+
+export default OnbordingScreen
+
+const styles = StyleSheet.create({})
